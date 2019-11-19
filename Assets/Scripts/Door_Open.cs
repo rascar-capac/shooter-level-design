ec708f6d38c5d843e3c97399c2f9d42db43671c4
9638_gdes (Student) <s_9638_gdes@PEDAGOGIQUE.LAN>
2019-11-19 12:34:54 +0100

    Add GameManager.prefab
    WIP DoorSystem

---
+++ Assets/Scripts/Door_Open.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door_Open : MonoBehaviour
+{
+    [SerializeField]
+
+    public GameObject DoorToOpen;
+    public Transform Target;
+    public float doorSpeed = 1;
+
+    Transform targetPosition;
+    bool doorOpening = false;
+    private void OnTriggerEnter(Collider col)
+    {
+        if (!doorOpening)
+        {
+            doorOpening = true;
+        }        
+    }
+    private void Update()
+    {
+        if (doorOpening)
+        {           
+            if (!(Vector3.Distance(transform.position, Target.position) < 0.001f))
+            {
+                DoorToOpen.transform.Translate(Vector3.right * Time.deltaTime * doorSpeed);
+            }
+        }
+    }
+}
