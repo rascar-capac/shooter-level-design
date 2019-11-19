bc7109e8e39715c3d124ef72b9d8d22fc80f4a4c
9638_gdes (Student) <s_9638_gdes@PEDAGOGIQUE.LAN>
2019-11-19 17:31:17 +0100

    Add sounds folder, WIP YardScene

---
+++ Assets/Scripts/PlaySound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+using System.Collections;
+
+public class PlaySound : MonoBehaviour
+{
+
+    public AudioClip SoundToPlay;
+    public float Volume;
+    AudioSource audio;
+    public bool alreadyPlayed = false;
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter()
+    {
+        if (!alreadyPlayed)
+        {
+            audio.PlayOneShot(SoundToPlay, Volume);
+            alreadyPlayed = true;
+        }
+    }
+}
