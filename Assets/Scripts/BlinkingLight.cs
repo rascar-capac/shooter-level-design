6781267a300dfa8c0d9c8fa401d629f4b423ece7
9638_gdes (Student) <s_9638_gdes@PEDAGOGIQUE.LAN>
2019-11-20 09:57:41 +0100

    WIP YardScene, Add NightLight.prefab, Add folder Interactive in prefabs folder

---
+++ Assets/Scripts/BlinkingLight.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkingLight : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector2 minimum;
+    public Vector2 maximum = new Vector2(15, 15);
+    [Range(0,3)]public float speed; 
+    Light light;
+    float t = 0;
+    bool isShrinking;
+
+    void Start()
+    {
+       light = GetComponent<Light>();
+       minimum = light.areaSize;
+       isShrinking = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(t);
+        if (!isShrinking)
+        {
+            light.areaSize = Vector2.Lerp(minimum, maximum, t += speed * Time.deltaTime);
+            if (t >= 1)
+            {
+                isShrinking = true;
+                t = 0;
+            }
+                
+        }
+        else
+        {
+            
+            light.areaSize = Vector2.Lerp(maximum, minimum, t += speed * Time.deltaTime);
+            if(t >= 1)
+            {
+                isShrinking = false;
+                t = 0;
+            }
+        }
+    }
+}
